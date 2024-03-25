872c581acf3d8c536c4d4a102af0eebe6cef4629
trungtin03 <trungtintran1103@gmail.com>
2024-03-25 15:47:23 +0700

    khai bao

--- BaiThiGK_63135728/app/src/main/java/ntu/mssv63135728/cau2_appbmi/MainActivity.java
+++ BaiThiGK_63135728/app/src/main/java/ntu/mssv63135728/cau2_appbmi/MainActivity.java
@@ -1,6 +1,10 @@
 package ntu.mssv63135728.cau2_appbmi;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etChieuCao;
+    EditText etCanNang;
+    RadioButton rbNam, rbNu;
+    Button btnTinhBMI;
+    TextView txtChiso, txtNhanXet;
+    double chiso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        etChieuCao = findViewById(R.id.et_chieucao);
+        etCanNang = findViewById(R.id.et_cannang);
+        rbNam = findViewById(R.id. btn_nam);
+        rbNu = findViewById(R.id.btn_nư);
+        btnTinhBMI = findViewById(R.id.btn_TinhBMI);
+        txtChiso = findViewById(R. id.txt_ketqua);
+        txtNhanXet = findViewById(R.id.txt_nhanxet);
+        txtChiso = findViewById(R.id.txt_ketqua);
+        txtNhanXet = findViewById(R.id. txt_nhanxet);
+
     }
 }
