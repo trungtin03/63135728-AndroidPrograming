0c8356dd7ac983f8d221b62f41ed2f94fcb8ca2e
trungtin03 <trungtintran1103@gmail.com>
2024-03-25 23:45:43 +0700

    khai bao

--- BaiThiGK_63135728/Cau3_appGioithieu/app/src/main/java/ntu/mssv63135728/appgioithieu/MainActivity.java
+++ BaiThiGK_63135728/Cau3_appGioithieu/app/src/main/java/ntu/mssv63135728/appgioithieu/MainActivity.java
@@ -7,18 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView nameTextView, ageTextView, hobbyTextView, genderTextView, majorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        nameTextView = findViewById(R.id.nameTextView);
+        ageTextView = findViewById(R.id.ageTextView);
+        hobbyTextView = findViewById(R.id.hobbyTextView);
+        genderTextView = findViewById(R.id.genderTextView);
+        majorTextView = findViewById(R.id.majorTextView);
+        Button showInfoButton = findViewById(R.id.showInfoButton);
     }
 }
