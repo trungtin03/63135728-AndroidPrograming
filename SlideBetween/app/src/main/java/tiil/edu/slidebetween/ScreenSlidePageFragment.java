dda02c1e116f7a5b18fb66caf1691daae6f5d22e
trungtin03 <trungtintran1103@gmail.com>
2024-04-16 17:38:10 +0700

    slide between fragment using viewpaper

---
+++ SlideBetween/app/src/main/java/tiil/edu/slidebetween/ScreenSlidePageFragment.java
@@ -0,0 +1,17 @@
+package tiil.edu.slidebetween;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen, container, false);
+    }
+}
