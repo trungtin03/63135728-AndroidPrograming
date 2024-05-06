aa6fd25bc388e85ac4514914773c78a8dd511912
trungtin03 <trungtintran1103@gmail.com>
2024-05-06 21:34:15 +0700

    update

--- ViDu Intent Dongian/app/src/main/java/tiil/edu/intent_dongian/SubActivityOne.java
+++ ViDu Intent Dongian/app/src/main/java/tiil/edu/intent_dongian/SubActivityOne.java
@@ -1,5 +1,19 @@
 package tiil.edu.intent_dongian;
 
-public class SubActivityOne {
-    
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivityOne extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInStanceState) {
+        super.onCreate(savedInStanceState);
+        setContentView(R.layout.activity_sub_one);
+    }
+    public void QuayVe(View v){
+        Intent iManHinhChinh = new Intent(this, MainActivity.class);
+        startActivity(iManHinhChinh);
+    }
 }
