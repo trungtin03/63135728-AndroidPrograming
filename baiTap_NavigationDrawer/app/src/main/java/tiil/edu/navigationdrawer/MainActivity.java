fe091af26743ff77ebcbdfcdfb564ce0dd11c447
trungtin03 <trungtintran1103@gmail.com>
2024-05-14 00:55:41 +0700

    Navigation fragment

---
+++ baiTap_NavigationDrawer/app/src/main/java/tiil/edu/navigationdrawer/MainActivity.java
@@ -0,0 +1,64 @@
+package tiil.edu.navigationdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        DrawerLayout drawerLayout = findViewById(R.id.my_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id1 = item.getItemId();
+                item.setChecked(true);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                if (id1 == R.id.nav_home) {
+                    replaceFragment(new HomeFragment());
+                } else if (id1 == R.id.nav_message) {
+                    replaceFragment(new MessageFragment());
+                } else if (id1 == R.id.nav_settings) {
+                    replaceFragment(new SettingFragment());
+                }
+                else {
+                    return true;
+                }
+                return true;
+            }
+
+        });
+    }
+    private void replaceFragment(Fragment f) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, f);
+        fragmentTransaction.commit();
+    }
+}
